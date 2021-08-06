8a17d3bb24b4f38ca8956009950a2c289d9999d8
Phil Hennel <pdiegmann@gmail.com>
2021-08-06 07:31:18 +0200

    Update landkreis_inzidenz.5m.sh

--- landkreis_inzidenz.5m.sh
+++ landkreis_inzidenz.5m.sh
@@ -13,8 +13,8 @@ export PATH="/opt/homebrew/bin:$PATH"
 export LC_NUMERIC="de_DE.UTF-8"
 export LANG="de_DE.UTF-8"
 
-curl -s "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?where=GEN%20%3D%20'$(jq -rn --arg x "$COUNTY" '$x|@uri')'&outFields=cases7_per_100k_txt&outSR=4326&f=json&returnGeometry=false" | jq ".features[]?.attributes.cases7_per_100k_txt" | cut -d "\"" -f 2
+curl -s -H 'Cache-Control: no-cache' "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?where=GEN%20%3D%20'$(jq -rn --arg x "$COUNTY" '$x|@uri')'&outFields=cases7_per_100k_txt&outSR=4326&f=json&returnGeometry=false&$(date +%s)" | jq ".features[]?.attributes.cases7_per_100k_txt" | cut -d "\"" -f 2
 
 echo "---"
-curl -s "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?where=GEN%20%3D%20'$(jq -rn --arg x "$COUNTY" '$x|@uri')'&outFields=cases7_bl_per_100k&outSR=4326&f=json&returnGeometry=false" | jq ".features[]?.attributes.cases7_bl_per_100k" | xargs printf "Landesweit: %0.1f\n"
-curl -s "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?where=GEN%20%3D%20'$(jq -rn --arg x "$COUNTY" '$x|@uri')'&outFields=last_update&outSR=4326&f=json&returnGeometry=false" | jq ".features[]?.attributes.last_update" | xargs printf "Aktualisiert: %s %s\n"
+curl -s -H 'Cache-Control: no-cache' "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?where=GEN%20%3D%20'$(jq -rn --arg x "$COUNTY" '$x|@uri')'&outFields=cases7_bl_per_100k&outSR=4326&f=json&returnGeometry=false&$(date +%s)" | jq ".features[]?.attributes.cases7_bl_per_100k" | xargs printf "Landesweit: %0.1f|href=https://experience.arcgis.com/experience/478220a4c454480e823b17327b2bf1d4\n"
+curl -s -H 'Cache-Control: no-cache' "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?where=GEN%20%3D%20'$(jq -rn --arg x "$COUNTY" '$x|@uri')'&outFields=last_update&outSR=4326&f=json&returnGeometry=false&$(date +%s)" | jq ".features[]?.attributes.last_update" | xargs printf "Aktualisiert: %s %s|refresh=true\n"
